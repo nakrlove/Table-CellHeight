fa9d0f3713cbc4889431b8166e371f021980fa91
nakrlove <nakrlove@gmail.com>
2022-11-11 01:40:25 +0900

    Initial Commit

---
+++ Table-CellHeight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table-CellHeight
+//
+//  Created by nakrlove on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
